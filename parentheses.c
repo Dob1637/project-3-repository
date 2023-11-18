09b389257d6797d85dabd6db934e7d0a6f378ff8
Brendon Do <brendondo1637@gmail.com>
2023-11-18 17:15:22 -0500

    added the malloc and the while loop for project.

--- parentheses.c
+++ parentheses.c
@@ -3,8 +3,19 @@
 
 int main(void){
 
-    
+    char *string;
+    size_t maxlen = 10;
+    int ret = EOF + 1;
 
+    string = (char *) malloc(sizeof(char)*maxlen);
+    ret = getline(&string, &maxlen, stdin);
 
+    while(ret != EOF){
+	printf("VALID\n");
+	ret = getline(&string, &maxlen, stdin);
+    }
+
+    printf("Used end of file input\n");
+    free(string);
     return 0;
 }
