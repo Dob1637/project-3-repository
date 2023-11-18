8f7071d90d5f412224d2db84f3c37496a986528d
Brendon Do <brendondo1637@gmail.com>
2023-11-18 18:01:55 -0500

    added if and else if statements in while loop, not working currently.

--- parentheses.c
+++ parentheses.c
@@ -1,21 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+int main(void)
+{
 
     char *string;
     size_t maxlen = 10;
     int ret = EOF + 1;
+    int paralen = 0;
+    int i;
+    char para_array[100];
 
     string = (char *) malloc(sizeof(char)*maxlen);
     ret = getline(&string, &maxlen, stdin);
 
-    while(ret != EOF){
-	printf("VALID\n");
+    while(ret != EOF)
+    {
+	for(i = 0; i < ret - 1; i++)
+	{
+	    if((string[i]) == ")" && para_array[paralen - 1] == "(")
+	    {
+		paralen -= 1;		
+	    }
+	    else if((string[i]) == "}" && para_array[paralen - 1] == "{")
+	    {
+		paralen -= 1;
+	    }
+	    else if((string[i]) == "]" && para_array[paralen - 1] == "[")
+	    {
+		paralen -= 1;
+	    }
+	    else if((string[i]) == "}" && para_array[paralen - 1] != "{")
+	    {
+	        paralen += 1;
+		break;
+	    }
+	    else if((string[i]) == ")" && para_array[paralen - 1] != "(")
+	    {
+		paralen += 1;
+	        break;
+	    }
+	    else if((string[i]) == "]" && para_array[paralen - 1] != "[")
+	    {
+	        paralen += 1;
+		break;
+	    }
+	    else if((string[i]) == "(" || string[i] == "{" || string[i] == "[")
+	    {
+		para_array[paralen] = string[i];
+		paralen += 1;
+	    }
+	}
+
+	if(paralen == 0)
+	{
+	    printf("VALID\n");
+	}
+	else
+	{
+	    printf("NOT VALID\n");
+	}
+	for(i=0; i<100; i++)
+	{
+	    para_array[i] = "0";
+	}
+	paralen = 0;
 	ret = getline(&string, &maxlen, stdin);
     }
 
-    printf("Used end of file input\n");
     free(string);
     return 0;
 }
